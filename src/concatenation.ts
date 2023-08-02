349d04d15bcd9c9dde958f5baa0aa58f9c111ef6
number-nine <sergii.pokhylko@gmail.com>
2023-08-02 17:25:47 +0300

    sand

---
+++ src/concatenation.ts
@@ -0,0 +1,13 @@
+function concatenation(firstWord: string, secondWord: string) {
+  console.log(`${firstWord} ${secondWord}`);
+}
+
+export { concatenation };
+
+type User = {
+  name: string;
+  age: number;
+};
+
+
+
